885719c5ea49654417ed18701a36e8992d083985
Alessandro Scardova <alessandro.scardova@mahiz.it>
2017-03-09 13:56:05 +0100

    Add project files.

---
+++ IndacoNews/IndacoNews/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IndacoNews
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnRefreshClicked(object sender, EventArgs e)
+        {
+            await GetLatestNews();
+        }
+
+        private async void OnAboutClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.InfoView());
+        }
+
+        private async void OnDownloadNewsClicked(object sender, EventArgs e)
+        {
+            await GetLatestNews();
+        }
+
+        private async Task GetLatestNews()
+        {
+            LoadingIndicator.IsVisible = true;
+            LoadingIndicator.IsRunning = true;
+
+            RssService service = new RssService();
+            IEnumerable<FeedItem> items = await service.GetNews("http://indaco.coop/feed/");
+            News.ItemsSource = items;
+            LoadingIndicator.IsRunning = false;
+        }
+
+        private void OnNewsSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            FeedItem item = e.SelectedItem as FeedItem;
+            if (item != null)
+            {
+                Device.OpenUri(new Uri(item.Link, UriKind.Absolute));
+            }
+        }
+
+    }
+}
---
+++ IndacoNews/IndacoNews/Services/RssService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IndacoNews.Services
+{
+    public class RssService
+    {
+        public async Task<IEnumerable<FeedItem>> GetNews(string url)
+        {
+            HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync(url);
+
+            var xdoc = XDocument.Parse(result);
+            return (from item in xdoc.Descendants("item")
+                    select new FeedItem()
+                    {
+                        Title = (string)item.Element("title"),
+                        Description = (string)item.Element("description"),
+                        Link = (string)item.Element("link"),
+                        PublishDate = DateTime.Parse((string)item.Element("pubDate"))
+                    }).ToList();
+        }
+    }
+}
